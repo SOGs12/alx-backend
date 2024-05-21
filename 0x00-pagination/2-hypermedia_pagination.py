b6a33297ed239f259f7c66a8be135dce339569c1
SOGs12 <mosesoyewumi05@gmail.com>
2024-05-21 15:42:06 +0000

    Hypermedia pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,25 +1,14 @@
 #!/usr/bin/env python3
-"""Hypermedia pagination generating script
-    """
-
-
-import requests
+"""Simple pagination"""
 import csv
-import math
+from math import ceil
 from typing import List
 
 
-def index_range(page, page_size):
-    """Return a tuple of size two
-    containing a start index and an end index
-
-    Args:
-        page (INT): corresponding to the range of indexes
-        page_size (INT): corresponding to the range of indexes
-    """
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size
-    return (start_index, end_index)
+def index_range(page: int, page_size: int) -> tuple:
+    """return a tuple of size two containing a start index and an end index"""
+    start = (page - 1) * page_size
+    return (start, start + page_size)
 
 
 class Server:
@@ -42,45 +31,28 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Return the appropriate page of the dataset
-        (i.e. the correct list of rows).
-
-        Args:
-            page (int, optional): Defaults to 1.
-            page_size (int, optional): Defaults to 10.
-
-        Returns:
-            List[List]: _description_
+        """finds the correct indexes to paginate the dataset \
+            correctly and return the appropriate page of the dataset
         """
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
         dataset = self.dataset()
-
-        assert isinstance(page, int)
-        assert isinstance(page_size, int)
-
-        assert page > 0
-        assert page_size > 0
-        try:
-            indexing = index_range(page, page_size)
-            return dataset[indexing[0]:indexing[1]]
-        except IndexException as e:
-            return []
+        return [] if (start >= len(dataset) or
+                      end >= len(dataset)) else dataset[start:end]
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
-        """returns a dictionary of hypermedia keys and values
+        """returns an hypermedia object based on self.get_page result"""
+        page_data = self.get_page(page, page_size)
+        total_pages = ceil(len(self.dataset()) / page_size)
+        next_page = page + 1 if page + 1 < total_pages else None
+        prev_page = page - 1 if page - 1 > 1 else None
 
-        Args:
-            page (int, optional):Defaults to 1.
-            page_size (int, optional):Defaults to 10.
-        """
-        data = self.get_page(page, page_size)
-        total_pages = len(self.dataset()) // page_size
-
-        dict = {
-            "page_size": page_size if page_size <= len(data) else len(data),
+        return {
+            "page_size": len(page_data),
             "page": page,
-            "data": data,
-            "next_page": page + 1 if page >= 0 else None,
-            "prev_page": page - 1 if page >= 1 else None,
+            "data": page_data,
+            "next_page": next_page,
+            "prev_page": prev_page,
             "total_pages": total_pages
-            }
-        return dict
+        }

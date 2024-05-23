e8e9c9c4767efbb52cb404aca357b86a08525e0f
SOGs12 <mosesoyewumi05@gmail.com>
2024-05-23 01:02:14 +0000

    caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,37 +1,40 @@
 #!/usr/bin/env python3
-"""A script for FIFO caching system
+""" BaseCaching module
 """
-
-
-from collections import deque
-BaseCaching = __import__('base_caching').BaseCaching
+from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """FIFO cache system"""
+    """
+    FIFOCache defines a FIFO caching system
+    """
 
     def __init__(self):
+        """
+        Initialize the class with the parent's init method
+        """
         super().__init__()
-        self.order = deque()
+        self.order = []
 
     def put(self, key, item):
-        """ put key value pair into cache"""
-        if (key is None or item is None):
-            return
-        capacity = BaseCaching.MAX_ITEMS
-        self.cache_data[key] = item
-        self.order.append(key)
-        if key in self.cache_data:
+        """
+        Cache a key-value pair
+        """
+        if key is None or item is None:
+            pass
+        else:
+            length = len(self.cache_data)
+            if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+                print("DISCARD: {}".format(self.order[0]))
+                del self.cache_data[self.order[0]]
+                del self.order[0]
+            self.order.append(key)
             self.cache_data[key] = item
-        if len(self.cache_data) >= capacity:
-            key_to_pop = self.order.popleft()
-            del self.cache_data[key_to_pop]
-            print(f"DISCARD: {key_to_pop}")
 
     def get(self, key):
-        """ return the value in self.cache_data linked to key.
-        If key is None or if the key doesn’t exist
-        in self.cache_data, return None."""
-        if key is None or key not in self.cache_data:
-            return None
-        return self.cache_data.get(key)
+        """
+        Return the value linked to a given key, or None
+        """
+        if key is not None and key in self.cache_data.keys():
+            return self.cache_data[key]
+        return None
